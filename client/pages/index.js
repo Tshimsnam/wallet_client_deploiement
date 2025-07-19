4f7af98553834853ea26b3e477bdda0334d00d72
Tshims <manassetshims@gmail.com>
2025-07-19 17:55:18 +0100

    redirect to login

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,9 +1,13 @@
+// pages/index.js
+export async function getServerSideProps() {
+  return {
+    redirect: {
+      destination: '/login',
+      permanent: false,
+    },
+  };
+}
+
 export default function Home() {
-  return (
-    <main className="min-h-screen flex items-center justify-center bg-gray-100">
-      <h1 className="text-3xl font-bold text-blue-600">
-        Tailwind fonctionne ✅
-      </h1>
-    </main>
-  );
+  return null;
 }
